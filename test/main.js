0c6dd894d99e1f5d5fc8bc07982f50aedda20272
Nathan Carter <ncarter@bentley.edu>
2020-07-13 15:49:26 -0400

    Add placeholder tests

---
+++ test/main.js
@@ -0,0 +1,11 @@
+
+let expect = require( 'expect.js' )
+
+suite( 'Main', () => {
+
+    test( 'Placeholder', () => {
+        // we'll add real tests later
+        expect( true ).to.be.ok()
+    } )
+
+} )
